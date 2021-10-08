1483fd2dd8719026f8851c8c8a0f8fe4e4078228
Tan <1983670147@qq.com>
2021-10-08 23:38:49 +0800

    登录页面

--- src/main/java/com/edu/controller/front/IndexController.java
+++ src/main/java/com/edu/controller/front/IndexController.java
@@ -19,4 +19,23 @@ public class IndexController {
         return "/index.html";
     }
 
+    //    这里的url不能跟上面的一样
+    @RequestMapping("/login")
+    public String loginAction(){
+        return "/login.html";
+    }
+//两个登录页面？？有一个是我自己做的，我觉得都差不多，记得调整
+    //先上传 Can't commit changes due to unresolved conflicts. 什么意思啊 等下
+
+    @RequestMapping("/login1")
+    public String loginaAction(){
+        return "/login1.html";
+    }
+
+    @RequestMapping("/zhuce")
+    public String zhuceAction(){
+        return "/zhuce.html";
+    }
+
+
 }
