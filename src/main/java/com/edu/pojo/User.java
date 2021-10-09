dce7d9d109661233b357aa2bb60fd9b6b4f46bf8
yz <1793925141@qq.com>
2021-10-09 10:27:12 +0800

    系统接口完成

--- src/main/java/com/edu/pojo/User.java
+++ src/main/java/com/edu/pojo/User.java
@@ -1,5 +1,7 @@
 package com.edu.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.util.Date;
@@ -15,57 +17,70 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@ApiModel("用户实体类")
 public class User {
 
     /**
      * 用户id
      */
+    @ApiModelProperty("用户id")
     private Integer user_id;
     /**
      * 用户名
      */
+    @ApiModelProperty("用户名")
     private String user_name;
     /**
      * 用户密码
      */
+    @ApiModelProperty("用户密码")
     private String pass_word;
     /**
      * 用户邮箱
      */
+    @ApiModelProperty("用户邮箱")
     private String user_email;
     /**
      * 用户电话
      */
+    @ApiModelProperty("用户电话")
     private String user_iphone;
     /**
      * 用户姓名
      */
+    @ApiModelProperty("用户姓名")
     private String name;
     /**
      * 用户性别
      */
+    @ApiModelProperty("用户性别")
     private String user_sex;
     /**
      * 用户状态
      */
+    @ApiModelProperty("用户状态")
     private String status;
     /**
      * 用户头像
      */
+    @ApiModelProperty("用户头像")
     private String user_image;
     /**
      * 用户注册时间
      */
+    @ApiModelProperty("用户注册时间")
     private Date register_time;
     /**
      * 用户最后一次登录时间
      */
+    @ApiModelProperty("用户最后一次登录时间")
     private Date finally_time;
 
 
     /**
      * 将时间转换成yy:dd/MM 。。。。。
      */
+    @ApiModelProperty("将时间转换")
     private String date;
 
 }
